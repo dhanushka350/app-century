334850e5fe3eb6ebc51878f32e39c63371883401
dhanushka350 <dhanushka@akvasoft.com>
2019-01-07 17:38:33 +0530

    added semantic, added side bar, added top bar, added navigation items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
